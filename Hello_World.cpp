4883490554609785e9a9a9a9e6b011bbd60a258b
Daniel Hiromoto <66335240+xTechon@users.noreply.github.com>
2021-08-06 14:49:36 -0400

    Hello_World

---
+++ Hello_World.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    cout << "Hello World"; // prints hello world
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Learning-C-plus-plus
 
 Trying to learn c++
+Using this tutorial:
+https://www.tutorialspoint.com/cplusplus/index.htm
