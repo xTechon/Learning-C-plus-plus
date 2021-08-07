6a91898b38c5697458ea7672a999aeacb1262de1
Daniel Hiromoto <66335240+xTechon@users.noreply.github.com>
2021-08-07 15:07:58 -0400

    Some functions

--- Binary_Ops.cpp
+++ Binary_Ops.cpp
@@ -4,6 +4,14 @@
 using namespace std;
 int C;
 void funct (string);
+/**
+ * "Function declaration is required when you define 
+ * a function in one source file and you call that 
+ * function in another file. In such case, you should 
+ * declare the function at the top of the file calling 
+ * the function."
+ */
+
 
 main(){
     int A = 60;
@@ -21,8 +29,17 @@ main(){
     C = A >> 1; //right shift bits
     funct("Right Shift");
 
+    //test to see if function has to  be declared at beginning like C
+    C = max(A,B); 
+    funct("test");
 }
 
 void funct (string op){
     cout << "Binary " << op << " is " << C << endl;
+}
+
+int max (int num1, int num2){
+    int result;
+    (num1 > num2) ? (result = num1) : (result = num2); //if else statment
+    return result;  
 }
